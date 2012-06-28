8a30a6acb46fe0dcc335a5c4084761833e5ef444
Benjamin Behr <benny@digitalbehr.de>
2012-06-28 08:57:34 +0200

    forgot to implement the create tempfile dep

--- test_application.rb
+++ test_application.rb
@@ -10,6 +10,16 @@ export TEST_APPLICATION_CONFIGURATION=#{ENV['APP_INSTALLER_TEST_APPLICATION_CONF
   }
 end
 
+dep 'create tempfile' do
+  met? {
+    File.exists?("/home/protonet/apps/test_application/test_file")
+  }
+  meet {
+    shell("mkdir -p /home/protonet/apps/test_application")
+    shell("touch /home/protonet/apps/test_application/test_file")
+  }
+end
+
 dep 'test_application install' do
   requires 'configuration', 'create tempfile'
 end
